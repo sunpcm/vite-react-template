d6d348eba11eab3e6a5118c3c6e5912f75c44ae7
sunpcm <sunpcm@163.com>
2025-09-15 16:27:46 +0800

    fix: add dynamic page title setting based on environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Dynamically set page title
+document.title = import.meta.env.VITE_APP_TITLE || 'My React Application'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
