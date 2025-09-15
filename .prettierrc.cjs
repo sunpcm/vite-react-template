40bcd275cb921787e4ff21a5509419fca8e31664
sunpcm <sunpcm@163.com>
2025-09-15 15:27:20 +0800

    chore: remove ESLint and Prettier configuration files; update package.json with engine requirements and add .nvmrc

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-// .prettierrc.cjs
-module.exports = {
-  semi: true, // 使用分号
-  singleQuote: true, // 使用单引号
-  trailingComma: 'all', // 在多行输入的尾部添加逗号
-  printWidth: 100, // 指定代码换行的行长
-  tabWidth: 2, // 指定每个缩进的空格数
-};
