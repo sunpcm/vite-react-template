775f617ea0ccab87670fe46e1f7f1004c6e94db2
sunpcm <sunpcm@163.com>
2025-09-19 10:02:18 +0800

    feat: add polyfills for TextEncoder and TextDecoder in Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
 // jest.setup.js
 import '@testing-library/jest-dom';
+
+// Polyfill for TextEncoder/TextDecoder (needed for React Router v7 and other modern libraries)
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
