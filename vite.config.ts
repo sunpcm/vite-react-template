05a73abb9b5d765e732f87de9806f0367f8ee792
sunpcm <sunpcm@163.com>
2025-12-09 22:42:20 +0800

    feat: add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import svgr from 'vite-plugin-svgr'; // <--- 导入
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,12 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src'),
     },
+  },
+  // Vitest 测试 config
+  test: {
+    globals: true, // 开启全局 API (describe, it, expect)，类似 Jest
+    environment: 'jsdom', // 模拟浏览器环境
+    setupFiles: './setupTests.ts', // 初始化文件
+    css: true, // 处理 CSS 导入
   },
 });
