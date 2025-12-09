08fd2718d15550fecd9ef0361dac73204bd87ac9
sunpcm <sunpcm@163.com>
2025-12-09 22:40:01 +0800

    test(test): remove the jest test

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// jest.setup.js
-import '@testing-library/jest-dom';
-
-// Polyfill for TextEncoder/TextDecoder (needed for React Router v7 and other modern libraries)
-import { TextEncoder, TextDecoder } from 'util';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
